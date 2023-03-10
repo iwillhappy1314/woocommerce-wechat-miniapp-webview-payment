06ee09625ecc0f849b95f5169e5bae2d29c5c8dc
Amos Lee <iwillhappy1314@gmail.com>
2023-03-10 16:27:50 +0800

    添加转发功能

--- pages/webview/webview.js
+++ pages/webview/webview.js
@@ -2,22 +2,29 @@ import config from '../../utils/config';
 
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-        src: config.getHomeUrl,
-    },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    src: config.getHomeUrl,
+  },
 
-    onLoad: function(options) {
-        console.log('webview接收到的参数', options);
+  onShareAppMessage() {
+    return {
+      title: config.getWebsiteName,
+      path: '/pages/webview/webview',
+    }
+  },
 
-        //如果支付成功，这里重新刷新h5页面，并把支付成功的状态传递给h5
-        if (options.src) {
-            this.setData({
-                src: decodeURI(options.src),
-            });
-        }
-    },
+  onLoad: function (options) {
+    console.log('webview接收到的参数', options);
+
+    //如果支付成功，这里重新刷新h5页面，并把支付成功的状态传递给h5
+    if (options.src) {
+      this.setData({
+        src: decodeURI(options.src),
+      });
+    }
+  },
 
 });
